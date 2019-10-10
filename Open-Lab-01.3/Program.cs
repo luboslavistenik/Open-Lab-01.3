07dea9eac608dcab569902f018df0c1d17aaa1ff
z18ista08 <z18ista08@Z00P1-18.SPSKNM>
2019-10-10 08:41:27 +0200

    Add project files.

---
+++ Open-Lab-01.3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Open_Lab_01._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9;
+            int b = a + 1;
+            Console.WriteLine(b);
+        }
+    }
+}
